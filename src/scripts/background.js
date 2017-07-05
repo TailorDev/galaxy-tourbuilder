4104e7f18bb87e0ed9088d720e5d3f478fb44907
William Durand <will+git@drnd.me>
2017-07-05 17:34:56 +0200

    Simplify extension toggling

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -8,49 +8,42 @@ type Tab = {
 
 let isActive: Array<boolean> = [];
 
-const toggle = (active: boolean, callback: Function) => {
-  ext.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const activeTab = tabs[0];
-    ext.tabs.sendMessage(
-      activeTab.id,
-      { action: ACTION_ENABLE, value: active },
-      callback
-    );
-  });
+const toggle = (active: boolean, tabId: number, callback: Function) => {
+  ext.tabs.sendMessage(
+    tabId,
+    { action: ACTION_ENABLE, value: active },
+    callback
+  );
 };
 
-const run = (active: boolean, tabId: number) => {
-  if (active) {
-    toggle(false, () => {
-      ext.browserAction.setIcon({
-        tabId,
-        path: {
-          '19': 'icons/disabled/icon-19.png',
-          '38': 'icons/disabled/icon-38.png',
-        },
-      });
+const run = (enable: boolean, tabId: number) => {
+  toggle(enable, tabId, () => {
+    ext.browserAction.setIcon({
+      tabId,
+      path: {
+        '19': enable ? 'icons/icon-19.png' : 'icons/disabled/icon-19.png',
+        '38': enable ? 'icons/icon-38.png' : 'icons/disabled/icon-38.png',
+      },
     });
-  } else {
-    toggle(true, () => {
-      ext.browserAction.setIcon({
-        tabId,
-        path: {
-          '19': 'icons/icon-19.png',
-          '38': 'icons/icon-38.png',
-        },
-      });
-    });
-  }
-
-  isActive[tabId] = !active;
+  });
 };
 
 ext.tabs.onUpdated.addListener((tabId: number, info) => {
   if (info.status === 'complete') {
-    run(!isActive[tabId], tabId);
+    if (isActive[tabId] === undefined) {
+      return;
+    }
+
+    run(isActive[tabId], tabId);
   }
 });
 
 ext.browserAction.onClicked.addListener((tab: Tab) => {
-  run(isActive[tab.id] || false, tab.id);
+  if (isActive[tab.id] === undefined) {
+    isActive[tab.id] = true;
+  } else {
+    isActive[tab.id] = !isActive[tab.id];
+  }
+
+  run(isActive[tab.id], tab.id);
 });
