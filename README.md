36cd8f69eba0893465ac82dbab6df3382b647d7d
William Durand <will+git@drnd.me>
2018-06-30 23:34:15 +0200

    Apply prettier on md files + more js files

--- README.md
+++ README.md
@@ -24,35 +24,36 @@
   <p>
 </div>
 
-[![Build Status](https://travis-ci.org/TailorDev/galaxy-tourbuilder.svg?branch=master)](https://travis-ci.org/TailorDev/galaxy-tourbuilder) [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.830481.svg)](https://doi.org/10.5281/zenodo.830481)
+[![Build Status](https://travis-ci.org/TailorDev/galaxy-tourbuilder.svg?branch=master)](https://travis-ci.org/TailorDev/galaxy-tourbuilder)
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.830481.svg)](https://doi.org/10.5281/zenodo.830481)
 
 ## Installation
 
-1. Clone this repository
-2. Run `npm install`
-3. Run `npm run build`
+1.  Clone this repository
+2.  Run `npm install`
+3.  Run `npm run build`
 
 **Note:** This is only useful if you want to install the extension locally,
-_e.g._, for hacking on it. If you want to use it, you should install it
-from your browser "app store".
+_e.g._, for hacking on it. If you want to use it, you should install it from
+your browser "app store".
 
 ##### Load the extension in Chrome & Opera
 
-1. Open Chrome/Opera browser and navigate to chrome://extensions
-2. Select "Developer Mode" and then click "Load unpacked extension..."
-3. From the file browser, choose to `galaxy-tourbuilder/build/chrome` or
-   (`galaxy-tourbuilder/build/opera`)
+1.  Open Chrome/Opera browser and navigate to chrome://extensions
+2.  Select "Developer Mode" and then click "Load unpacked extension..."
+3.  From the file browser, choose to `galaxy-tourbuilder/build/chrome` or
+    (`galaxy-tourbuilder/build/opera`)
 
 ##### Load the extension in Firefox
 
-1. Open Firefox browser and navigate to about:debugging
-2. Click "Load Temporary Add-on" and from the file browser, choose
-   `galaxy-tourbuilder/build/firefox`
+1.  Open Firefox browser and navigate to about:debugging
+2.  Click "Load Temporary Add-on" and from the file browser, choose
+    `galaxy-tourbuilder/build/firefox`
 
 ## Developing
 
 The following tasks can be used when you want to start developing the extension
-and want to enable live reload -
+and want to enable live reload:
 
 - `npm run chrome-watch`
 - `npm run opera-watch`
@@ -65,32 +66,32 @@ browser. You can then upload that to the different app stores.
 
 ##### Chrome Web Store
 
-This extension is available under the name [Galaxy Tour
-Builder](https://chrome.google.com/webstore/detail/galaxy-tour-builder/mdfbapknmcpnbmggahhaegehbbbmhmgg)
+This extension is available under the name
+[Galaxy Tour Builder](https://chrome.google.com/webstore/detail/galaxy-tour-builder/mdfbapknmcpnbmggahhaegehbbbmhmgg)
 in the Chrome Web Store.
 
 #### Mozilla Add-ons (Firefox)
 
-This extension is available under the name [Galaxy Tour
-Builder](https://addons.mozilla.org/en-US/firefox/addon/galaxy-tour-builder/) on
-the Mozilla Add-on website.
+This extension is available under the name
+[Galaxy Tour Builder](https://addons.mozilla.org/en-US/firefox/addon/galaxy-tour-builder/)
+on the Mozilla Add-on website.
 
 #### Opera add-ons
 
-This extension is available under the name [Galaxy Tour
-Builder](https://addons.opera.com/en/extensions/details/galaxy-tour-builder/) on
-the Opera add-ons website.
+This extension is available under the name
+[Galaxy Tour Builder](https://addons.opera.com/en/extensions/details/galaxy-tour-builder/)
+on the Opera add-ons website.
 
 ## Credits
 
 This project has been initiated during a hackathon a while ago in Freiburg, then
 [Dannon Baker](https://github.com/dannon) started a Chrome extension
-[port](https://github.com/dannon/tourbuilder). In no particular order, [Florian
-Eggenhofer](https://github.com/eggzilla), [Matthias
-Bernt](https://github.com/bernt-matthias), [Bérénice
-Batut](https://github.com/bebatut) and [Clemens
-Blank](https://github.com/blankclemens) have contributed to this port and
-therefore to this project.
+[port](https://github.com/dannon/tourbuilder). In no particular order,
+[Florian Eggenhofer](https://github.com/eggzilla),
+[Matthias Bernt](https://github.com/bernt-matthias),
+[Bérénice Batut](https://github.com/bebatut) and
+[Clemens Blank](https://github.com/blankclemens) have contributed to this port
+and therefore to this project.
 
 ## License
 
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,17 @@
-import fs from "fs";
+import fs from 'fs';
 import gulp from 'gulp';
-import {merge} from 'event-stream'
+import { merge } from 'event-stream';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
 import preprocessify from 'preprocessify';
-import gulpif from "gulp-if";
+import gulpif from 'gulp-if';
 
 const $ = require('gulp-load-plugins')();
 
-var production = process.env.NODE_ENV === "production";
-var target = process.env.TARGET || "chrome";
-var environment = process.env.NODE_ENV || "development";
+var production = process.env.NODE_ENV === 'production';
+var target = process.env.TARGET || 'chrome';
+var environment = process.env.NODE_ENV || 'development';
 
 var generic = JSON.parse(fs.readFileSync(`./config/${environment}.json`));
 var specific = JSON.parse(fs.readFileSync(`./config/${target}.json`));
@@ -19,36 +19,36 @@ var context = Object.assign({}, generic, specific);
 
 var manifest = {
   dev: {
-    "background": {
-      "scripts": [
-        "scripts/livereload.js",
-        "scripts/background.js"
-      ]
-    }
+    background: {
+      scripts: ['scripts/livereload.js', 'scripts/background.js'],
+    },
   },
 
   firefox: {
-    "applications": {
-      "gecko": {
-        "id": "{b88158d8-6607-4404-8284-b904c58b5b02}"
-      }
-    }
-  }
-}
+    applications: {
+      gecko: {
+        id: '{b88158d8-6607-4404-8284-b904c58b5b02}',
+      },
+    },
+  },
+};
 
 // Tasks
 gulp.task('clean', () => {
-  return pipe(`./build/${target}`, $.clean())
-})
+  return pipe(
+    `./build/${target}`,
+    $.clean()
+  );
+});
 
 gulp.task('build', (cb) => {
-  $.runSequence('clean', 'styles', 'ext', cb)
+  $.runSequence('clean', 'styles', 'ext', cb);
 });
 
 gulp.task('watch', ['build'], () => {
   $.livereload.listen();
 
-  gulp.watch(['./src/**/*']).on("change", () => {
+  gulp.watch(['./src/**/*']).on('change', () => {
     $.runSequence('build', $.livereload.reload);
   });
 });
@@ -56,104 +56,136 @@ gulp.task('watch', ['build'], () => {
 gulp.task('default', ['build']);
 
 gulp.task('ext', ['manifest', 'js'], () => {
-  return mergeAll(target)
+  return mergeAll(target);
 });
-
 
 // -----------------
 // COMMON
 // -----------------
 gulp.task('js', () => {
-  return buildJS(target)
-})
+  return buildJS(target);
+});
 
 gulp.task('styles', () => {
-  return gulp.src('src/styles/**/*.scss')
+  return gulp
+    .src('src/styles/**/*.scss')
     .pipe($.plumber())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
+    .pipe(
+      $.sass
+        .sync({
+          outputStyle: 'expanded',
+          precision: 10,
+          includePaths: ['.'],
+        })
+        .on('error', $.sass.logError)
+    )
     .pipe(gulp.dest(`build/${target}/styles`));
 });
 
-gulp.task("manifest", () => {
-  return gulp.src('./manifest.json')
-    .pipe(gulpif(!production, $.mergeJson({
-      fileName: "manifest.json",
-      jsonSpace: " ".repeat(4),
-      endObj: manifest.dev
-    })))
-    .pipe(gulpif(target === "firefox", $.mergeJson({
-      fileName: "manifest.json",
-      jsonSpace: " ".repeat(4),
-      endObj: manifest.firefox
-    })))
-    .pipe(gulp.dest(`./build/${target}`))
+gulp.task('manifest', () => {
+  return gulp
+    .src('./manifest.json')
+    .pipe(
+      gulpif(
+        !production,
+        $.mergeJson({
+          fileName: 'manifest.json',
+          jsonSpace: ' '.repeat(4),
+          endObj: manifest.dev,
+        })
+      )
+    )
+    .pipe(
+      gulpif(
+        target === 'firefox',
+        $.mergeJson({
+          fileName: 'manifest.json',
+          jsonSpace: ' '.repeat(4),
+          endObj: manifest.firefox,
+        })
+      )
+    )
+    .pipe(gulp.dest(`./build/${target}`));
 });
-
-
 
 // -----------------
 // DIST
 // -----------------
 gulp.task('dist', (cb) => {
-  $.runSequence('build', 'zip', cb)
+  $.runSequence('build', 'zip', cb);
 });
 
 gulp.task('zip', () => {
-  return pipe(`./build/${target}/**/*`, $.zip(`${target}.zip`), './dist')
-})
-
+  return pipe(
+    `./build/${target}/**/*`,
+    $.zip(`${target}.zip`),
+    './dist'
+  );
+});
 
 // Helpers
 function pipe(src, ...transforms) {
   return transforms.reduce((stream, transform) => {
-    const isDest = typeof transform === 'string'
-    return stream.pipe(isDest ? gulp.dest(transform) : transform)
-  }, gulp.src(src))
+    const isDest = typeof transform === 'string';
+    return stream.pipe(isDest ? gulp.dest(transform) : transform);
+  }, gulp.src(src));
 }
 
 function mergeAll(dest) {
   return merge(
-    pipe('./src/icons/**/*', `./build/${dest}/icons`),
-    pipe(['./src/_locales/**/*'], `./build/${dest}/_locales`),
-    pipe([`./src/images/${target}/**/*`], `./build/${dest}/images`),
-    pipe(['./src/images/shared/**/*'], `./build/${dest}/images`),
-    pipe(['./src/**/*.html'], `./build/${dest}`)
-  )
+    pipe(
+      './src/icons/**/*',
+      `./build/${dest}/icons`
+    ),
+    pipe(
+      ['./src/_locales/**/*'],
+      `./build/${dest}/_locales`
+    ),
+    pipe(
+      [`./src/images/${target}/**/*`],
+      `./build/${dest}/images`
+    ),
+    pipe(
+      ['./src/images/shared/**/*'],
+      `./build/${dest}/images`
+    ),
+    pipe(
+      ['./src/**/*.html'],
+      `./build/${dest}`
+    )
+  );
 }
 
 function buildJS(target) {
-  const files = [
-    'background.js',
-    'contentscript.js',
-    'livereload.js'
-  ]
+  const files = ['background.js', 'contentscript.js', 'livereload.js'];
 
-  let tasks = files.map( file => {
+  let tasks = files.map((file) => {
     return browserify({
       entries: 'src/scripts/' + file,
-      debug: true
+      debug: true,
     })
-    .transform('babelify')
-    .transform(preprocessify, {
-      includeExtensions: ['.js'],
-      context: context
-    })
-    .bundle()
-    .pipe(source(file))
-    .pipe(buffer())
-    .pipe(gulpif(!production, $.sourcemaps.init({ loadMaps: true }) ))
-    .pipe(gulpif(!production, $.sourcemaps.write('./') ))
-    .pipe(gulpif(production, $.uglify({
-      "mangle": false,
-      "output": {
-        "ascii_only": true
-      }
-    })))
-    .pipe(gulp.dest(`build/${target}/scripts`));
+      .transform('babelify')
+      .transform(preprocessify, {
+        includeExtensions: ['.js'],
+        context: context,
+      })
+      .bundle()
+      .pipe(source(file))
+      .pipe(buffer())
+      .pipe(gulpif(!production, $.sourcemaps.init({ loadMaps: true })))
+      .pipe(gulpif(!production, $.sourcemaps.write('./')))
+      .pipe(
+        gulpif(
+          production,
+          $.uglify({
+            mangle: false,
+            output: {
+              ascii_only: true,
+            },
+          })
+        )
+      )
+      .pipe(gulp.dest(`build/${target}/scripts`));
   });
 
   return merge.apply(null, tasks);
